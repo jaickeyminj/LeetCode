d74c713c567b3505f094bd422b5a455e854df93d
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-14 16:18:44 +0530

    Time: 9 ms (26.61%), Space: 42.3 MB (82.99%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -1,8 +1,32 @@
 class Solution {
+    // public int canBeTypedWords(String text, String brokenLetters) {
+    //     Set<Character> set=new HashSet<>();
+    //     for (char brokenLetter: brokenLetters.toCharArray()){
+    //         set.add(brokenLetter);
+    //     }
+    //     boolean flag=true;
+    //     int res=0;
+    //     for (char ch:text.toCharArray()){
+    //         if (ch==' '){
+    //             if (flag){
+    //                 res++;
+    //             } else {
+    //                 flag=true;
+    //             }
+    //         }
+    //         if (set.contains(ch)){
+    //             flag=false;
+    //         }
+    //     }
+    //     if (flag){
+    //         res+=1;
+    //     }
+    //     return res;
+    // }
     public int canBeTypedWords(String text, String brokenLetters) {
-        Set<Character> set=new HashSet<>();
+        int map[]= new int[26];
         for (char brokenLetter: brokenLetters.toCharArray()){
-            set.add(brokenLetter);
+            map[(brokenLetter)-'a']=1;
         }
         boolean flag=true;
         int res=0;
@@ -13,8 +37,9 @@ class Solution {
                 } else {
                     flag=true;
                 }
+                continue;
             }
-            if (set.contains(ch)){
+            if(map[(ch)-'a']==1){
                 flag=false;
             }
         }
