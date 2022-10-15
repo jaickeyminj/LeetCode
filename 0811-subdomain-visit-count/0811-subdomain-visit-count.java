f5a3347bc7c0f8bc0d8b76625b557b373cd52d57
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-15 12:04:26 +0530

    Time: 28 ms (74.39%), Space: 50.8 MB (68.24%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.java
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.java
@@ -2,14 +2,16 @@ class Solution {
     public List<String> subdomainVisits(String[] cpdomains) {
         HashMap<String,Integer> hm = new HashMap<>();
         for(String a: cpdomains){
-            String split[] = a.split("\\s+");
-            hm.put(split[1],hm.getOrDefault(split[1],0)+Integer.parseInt(split[0]));
+            int index = a.indexOf(' ');
+            int visit = Integer.parseInt(a.substring(0, index));
+            String domain = a.substring(index + 1);
+            hm.put(domain,hm.getOrDefault(domain,0)+(visit));
             
-            for(int i=0;i<split[1].length();i+=1){
-                if(split[1].charAt(i)=='.'){
-                            hm.put(split[1].substring(i+1),
-                                   hm.getOrDefault(split[1].substring(i+1),0)+
-                                   Integer.parseInt(split[0]));
+            for(int i=0;i<domain.length();i+=1){
+                if(domain.charAt(i)=='.'){
+                            hm.put(domain.substring(i+1),
+                                   hm.getOrDefault(domain.substring(i+1),0)+
+                                   (visit));
                 }
             }
         }
