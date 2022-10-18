74bd9a45d8305b0c801346a2dfe592d42c086f89
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-18 11:39:17 +0530

    Time: 10 ms (70.28%), Space: 44.9 MB (67.33%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -1,15 +1,26 @@
 class Solution {
+    // public String kthDistinct(String[] arr, int k) {
+    //     Map<String,Integer> map = new LinkedHashMap<>();
+    //     for(String s: arr){
+    //         map.put(s,map.getOrDefault(s,0)+1);
+    //     }
+    //     int i=0;
+    //     for(Map.Entry<String,Integer> entry: map.entrySet()){
+    //         if(entry.getValue()==1) i+=1;
+    //         if(i==k)
+    //             return entry.getKey();
+    //     }
+    //     return "";
+    // }
     public String kthDistinct(String[] arr, int k) {
-        Map<String,Integer> map = new LinkedHashMap<>();
-        for(String s: arr){
-            map.put(s,map.getOrDefault(s,0)+1);
-        }
-        int i=0;
-        for(Map.Entry<String,Integer> entry: map.entrySet()){
-            if(entry.getValue()==1) i+=1;
-            if(i==k)
-                return entry.getKey();
-        }
-        return "";
-    }
+		Map<String, Boolean> map = new HashMap<>();
+		for (String s : arr) {
+			if (map.containsKey(s)) map.put(s, false);
+			else map.put(s, true);
+		}
+		for (String s : arr) {
+			if (map.get(s) && k-- == 1) return s;
+		}
+		return "";
+	}
 }
