1e512d6aa5d17d5dab8b039b5690245d753c182b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-27 12:33:28 +0530

    Time: 2 ms (65.38%), Space: 40.6 MB (87.98%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,27 +1,27 @@
 class Solution {
-    // public String generateTheString(int n) {
-    //     if(n%2!=0){
-    //         StringBuilder sb = new StringBuilder();
-    //         for(int i=0;i<n;i+=1){
-    //             sb.append('c');
-    //         }
-    //         return sb.toString();
-    //     }
-    //     StringBuilder sb = new StringBuilder();
-    //     int i=1;
-    //     sb.append('a');
-    //     for(;i<n;i+=1){
-    //         sb.append('g');
-    //     }
-    //     return sb.toString();
-    // }
     public String generateTheString(int n) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i+=1){
-            sb.append('c');
-        }
-        if(n%2!=0)
+        if(n%2!=0){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<n;i+=1){
+                sb.append('c');
+            }
             return sb.toString();
-        return sb.toString().substring(1) + "o";
+        }
+        StringBuilder sb = new StringBuilder();
+        int i=1;
+        sb.append('a');
+        for(;i<n;i+=1){
+            sb.append('g');
+        }
+        return sb.toString();
     }
+    // public String generateTheString(int n) {
+    //     StringBuilder sb = new StringBuilder();
+    //     for(int i=0;i<n;i+=1){
+    //         sb.append('c');
+    //     }
+    //     if(n%2!=0)
+    //         return sb.toString();
+    //     return sb.toString().substring(1) + "o";
+    // }
 }
