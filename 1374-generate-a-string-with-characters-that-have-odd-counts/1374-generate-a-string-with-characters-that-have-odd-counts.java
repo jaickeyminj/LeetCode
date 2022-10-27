ef91510142d62f4d4052935e1bf231e9034307e2
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-27 12:29:56 +0530

    Time: 1 ms (96.31%), Space: 39.4 MB (97.76%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String generateTheString(int n) {
+        if(n%2!=0){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<n;i+=1){
+                sb.append('c');
+            }
+            return sb.toString();
+        }
+        StringBuilder sb = new StringBuilder();
+        int i=1;
+        sb.append('a');
+        for(;i<n;i+=1){
+            sb.append('g');
+        }
+        return sb.toString();
+    }
+}
