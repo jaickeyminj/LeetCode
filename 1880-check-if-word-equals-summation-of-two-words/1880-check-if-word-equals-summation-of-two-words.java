f3f6ed11881ca77dad53a471c0a49f91954bcd43
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-27 21:47:09 +0530

    Time: 1 ms (76.28%), Space: 42.2 MB (37.99%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.java
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.java
@@ -1,19 +1,30 @@
 class Solution {
+    // public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+    //     StringBuilder a,b,cc;
+    //     a = new StringBuilder();
+    //     b = new StringBuilder();
+    //     cc = new StringBuilder();
+    //     for(char c: firstWord.toCharArray()){
+    //         a.append((c-97)+"");
+    //     }
+    //     for(char c: secondWord.toCharArray()){
+    //         b.append((c-97)+"");
+    //     }
+    //     for(char c: targetWord.toCharArray()){
+    //         cc.append((c-97)+"");
+    //     }
+    //     return Integer.parseInt(a.toString())+Integer.parseInt(b.toString()) 
+    //         == Integer.parseInt(cc.toString());
+    // }
+    
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        StringBuilder a,b,cc;
-        a = new StringBuilder();
-        b = new StringBuilder();
-        cc = new StringBuilder();
-        for(char c: firstWord.toCharArray()){
-            a.append((c-97)+"");
-        }
-        for(char c: secondWord.toCharArray()){
-            b.append((c-97)+"");
-        }
-        for(char c: targetWord.toCharArray()){
-            cc.append((c-97)+"");
-        }
-        return Integer.parseInt(a.toString())+Integer.parseInt(b.toString()) 
-            == Integer.parseInt(cc.toString());
+        return getIntValue(firstWord) + getIntValue(secondWord) == getIntValue(targetWord);
     }
+    private int getIntValue(String s) {
+        int result = 0;
+        for(int i=0;i<s.length();i++)
+            result = result * 10 + (s.charAt(i) - 'a');
+        return result;
+    }
+    
 }
