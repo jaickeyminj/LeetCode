78f092f2c1f38cef58cd2c47fe1c9fcaeb586016
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-27 12:32:38 +0530

    Time: 2 ms (65.38%), Space: 41.3 MB (74.68%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,18 +1,27 @@
 class Solution {
+    // public String generateTheString(int n) {
+    //     if(n%2!=0){
+    //         StringBuilder sb = new StringBuilder();
+    //         for(int i=0;i<n;i+=1){
+    //             sb.append('c');
+    //         }
+    //         return sb.toString();
+    //     }
+    //     StringBuilder sb = new StringBuilder();
+    //     int i=1;
+    //     sb.append('a');
+    //     for(;i<n;i+=1){
+    //         sb.append('g');
+    //     }
+    //     return sb.toString();
+    // }
     public String generateTheString(int n) {
-        if(n%2!=0){
-            StringBuilder sb = new StringBuilder();
-            for(int i=0;i<n;i+=1){
-                sb.append('c');
-            }
-            return sb.toString();
-        }
         StringBuilder sb = new StringBuilder();
-        int i=1;
-        sb.append('a');
-        for(;i<n;i+=1){
-            sb.append('g');
+        for(int i=0;i<n;i+=1){
+            sb.append('c');
         }
-        return sb.toString();
+        if(n%2!=0)
+            return sb.toString();
+        return sb.toString().substring(1) + "o";
     }
 }
