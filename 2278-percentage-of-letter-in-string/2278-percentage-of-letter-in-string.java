fc77a21212987a6cd3a406101105f1fd76a98673
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-29 09:37:03 +0530

    Time: 1 ms (72.78%), Space: 42.1 MB (38.37%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
@@ -4,11 +4,8 @@ class Solution {
         for(char c: s.toCharArray())
             if(c==letter)
                 count+=1;
-        // System.out.println(count);
         double len = s.length();
         double per = (count/len) *100;
-        // System.out.println(per);
-        // System.out.println( per * 100);
         return (int) per;
     }
 }
