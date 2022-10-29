553b8a63ab3b2d716e872b38ed72590b3942f61e
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-29 10:02:36 +0530

    Time: 30 ms (27.45%), Space: 46.1 MB (67.23%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        
+        licensePlate = licensePlate.replaceAll("\\s+","");
+        licensePlate = licensePlate.replaceAll("([0-9])","");
+        licensePlate = licensePlate.replaceAll("([A-Z])", "$1").toLowerCase();
+        int map[] = new int[26];
+        for(char c: licensePlate.toCharArray()){
+            map[c-'a']+=1;
+        }
+        Arrays.sort(words, Comparator.comparing(s->s.length()));
+        String a="";
+        for(String word:words){
+            if(word.length()>=licensePlate.length()){
+                if(processAlpha(word,map)){
+                    a=word;
+                    return word;
+                }
+            }
+        }
+        return a;
+    }
+    public boolean processAlpha(String word, int map[]){
+        int map2[] = new int[26];
+        
+        for(char c: word.toCharArray()){
+            map2[c-'a']+=1;
+        }
+        for(int i=0;i<26;i+=1){
+            if(map[i]!=0 && map[i]>map2[i])
+                return false;
+        }
+        
+        return true;
+    }
+}
