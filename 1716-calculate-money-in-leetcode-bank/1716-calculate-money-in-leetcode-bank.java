13a5ced3c54bbcdd0a09f0b5ca8bbdf41d1204a8
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-10-31 11:12:54 +0530

    Time: 1 ms (79.50%), Space: 40.8 MB (59.68%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,15 +1,28 @@
 class Solution {
-    public int totalMoney(int n) {
-        int monday = 1, cur = 1, bank = 0;
-        for(int i = 1; i <= n; i++) {
-                bank += cur;
-                cur++;
-            if (i % 7 == 0) {
-                monday+=1;
-                cur = monday;
-            }
+//     public int totalMoney(int n) {
+//         int monday = 1, cur = 1, bank = 0;
+//         for(int i = 1; i <= n; i++) {
+//                 bank += cur;
+//                 cur++;
+//             if (i % 7 == 0) {
+//                 monday+=1;
+//                 cur = monday;
+//             }
             
+//         }
+//         return bank;
+//     }
+    public int totalMoney(int n) {
+        int fullWeek = n/7;
+        int res = 0;
+        for(int i=1;i<=fullWeek;i++){
+            res+= 7*(i+3);
         }
-        return bank;
+        
+        for(int i=7*fullWeek; i<n;i++){
+            fullWeek+=1;
+            res+=fullWeek;
+        }
+        return res;
     }
 }
