969ed0623e78dbf7b6baa4d4676a2fa22c86f054
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-02 13:38:00 +0530

    Time: 25 ms (44.26%), Space: 54.7 MB (91.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // Arrays.sort(nums);
+        // // System.out.println(Arrays.toString(nums));
+        // for(int i=0;i<nums.length-1;i++){
+        //     // System.out.println(nums[i]+" "+nums[i+1]);
+        //     if(nums[i]==nums[i+1]) return true;
+        // }
+        // return false;
+        Set<Integer> set = new TreeSet<>();
+        for(int i: nums){
+            if(set.contains(i)) return true;
+            set.add(i);
+        }
+        return false;
+    }
+}
