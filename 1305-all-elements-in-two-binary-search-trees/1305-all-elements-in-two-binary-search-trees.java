8ed0070c6cdb014af78ee1c6f8415e11adf3878b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-21 19:39:08 +0530

    Time: 37 ms (62.07%), Space: 67.5 MB (82.38%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,18 +14,38 @@
  * }
  */
 class Solution {
+//     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+//         List<Integer> ans = new ArrayList<>();
+//         inorder(root1,ans);
+//         inorder(root2,ans);
+//         Collections.sort(ans);
+//         return ans;
+//     }
+//     public void inorder(TreeNode root,List<Integer> a){
+//         if(root == null) return;
+        
+//         inorder(root.left,a);
+//         a.add(root.val);
+//         inorder(root.right,a);
+//     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Queue<Integer> q1 = new LinkedList<>(), q2 = new LinkedList<>();
+        inorder(root1, q1);
+        inorder(root2, q2);
         List<Integer> ans = new ArrayList<>();
-        inorder(root1,ans);
-        inorder(root2,ans);
-        Collections.sort(ans);
+        while (!q1.isEmpty() || !q2.isEmpty())
+            if (q2.isEmpty())
+                ans.add(q1.poll());
+            else if (q1.isEmpty())
+                ans.add(q2.poll());
+            else
+                ans.add(q1.peek() < q2.peek() ? q1.poll() : q2.poll());
         return ans;
     }
-    public void inorder(TreeNode root,List<Integer> a){
-        if(root == null) return;
-        
-        inorder(root.left,a);
-        a.add(root.val);
-        inorder(root.right,a);
+    private void inorder(TreeNode n, Queue<Integer> q) {
+        if (n == null) return;
+        inorder(n.left, q);
+        q.offer(n.val);
+        inorder(n.right, q);
     }
 }
