241abc98b2e9e10a28f1198534108309e873235b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-21 19:27:46 +0530

    Time: 0 ms (100.00%), Space: 65 MB (73.97%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -14,17 +14,23 @@
  * }
  */
 class Solution {
-    public int sum =0;
-    public int rangeSumBST(TreeNode root, int low, int high) {
-        inorder(root,low,high);
-        return sum;
-    }
+//     public int sum =0;
+//     public int rangeSumBST(TreeNode root, int low, int high) {
+//         inorder(root,low,high);
+//         return sum;
+//     }
     
-    public void inorder(TreeNode root,int low,int high){
-        if(root==null) return;
-        inorder(root.left,low,high);
-        if(root.val>=low && root.val<=high)
-            sum+=root.val;
-        inorder(root.right,low,high);
+//     public void inorder(TreeNode root,int low,int high){
+//         if(root==null) return;
+//         inorder(root.left,low,high);
+//         if(root.val>=low && root.val<=high)
+//             sum+=root.val;
+//         inorder(root.right,low,high);
+//     }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0; // base case.
+        if (root.val < low) return rangeSumBST(root.right, low, high); // left branch excluded.
+        if (root.val > high) return rangeSumBST(root.left, low, high); // right branch excluded.
+        return root.val + rangeSumBST(root.right, low, high) + rangeSumBST(root.left,low,high); // count in both children.
     }
 }
