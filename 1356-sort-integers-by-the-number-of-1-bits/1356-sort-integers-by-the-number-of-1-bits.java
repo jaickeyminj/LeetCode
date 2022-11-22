47a0e7e7bb11760b54375d0723916d1c45165fc3
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-22 13:46:16 +0530

    Time: 4 ms (94.69%), Space: 42.7 MB (88.27%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        for(int i=0; i<arr.length;i+=1){
+            arr[i] += countSetBits(arr[i]) * 10001;
+        }
+        // System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length;i+=1){
+            arr[i] %=  10001;
+        }
+        return arr;
+    }
+    private static int countSetBits(int N){
+        int count = 0;
+        for (int i = 0; i < 4 * 8; i++){
+            if ((N & (1 << i)) != 0)
+                count++;
+        }
+        return count;
+  }
+}
