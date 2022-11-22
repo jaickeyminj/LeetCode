2d9a3fef4f05a8a329fe0ca482ba176cf2b752a5
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-22 23:42:18 +0530

    Time: 51 ms (32.67%), Space: 68.4 MB (74.40%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -34,10 +34,10 @@ class Solution {
         inorder(root2, q2);
         List<Integer> ans = new ArrayList<>();
         while (!q1.isEmpty() || !q2.isEmpty())
-            if (q2.isEmpty())
-                ans.add(q1.poll());
-            else if (q1.isEmpty())
+            if (q1.isEmpty())
                 ans.add(q2.poll());
+            else if (q2.isEmpty())
+                ans.add(q1.poll());
             else
                 ans.add(q1.peek() < q2.peek() ? q1.poll() : q2.poll());
         return ans;
