0b3211cad22ae6d70004ab0354ed49760f34f3c9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-23 17:48:31 +0530

    Time: 2 ms (90.19%), Space: 43.7 MB (70.09%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,30 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int val =0;
+    //https://www.youtube.com/watch?v=cOFAmaMBVps
+//     public int singleNumber(int[] nums) {
+//         int val =0;
         
-        for(int i=0;i<32;i+=1){
-            int count =0;
-            for(int j=0;j<nums.length;j++){
-                if((nums[j]&(1<<i))!=0){
-                    count+=1;
-                }
-            }
-            if(count%3==1)
-                val+=1<<i;
+//         for(int i=0;i<32;i+=1){
+//             int count =0;
+//             for(int j=0;j<nums.length;j++){
+//                 if((nums[j]&(1<<i))!=0){
+//                     count+=1;
+//                 }
+//             }
+//             if(count%3==1)
+//                 val+=1<<i;
+//         }
+//         return val;
+//     }
+    public int singleNumber(int[] nums) {
+        int ones=0;
+        int two=0;
+        
+        for(int i:nums){
+            ones = ones^ i;
+            ones = ones& ~two;
+            two = i^two;
+            two = two & ~ones;
         }
-        return val;
+        return ones;
     }
 }
