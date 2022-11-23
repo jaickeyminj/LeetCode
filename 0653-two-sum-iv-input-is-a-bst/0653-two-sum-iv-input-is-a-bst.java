9e253e283f34ae47bccb892c44a2cb4202f39104
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-23 14:40:21 +0530

    Time: 3 ms (96.10%), Space: 43.2 MB (87.01%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,16 +14,36 @@
  * }
  */
 class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        Set<Integer> set = new HashSet<>();
+//     public boolean findTarget(TreeNode root, int k) {
+//         Set<Integer> set = new HashSet<>();
         
-        return findMe(root,k,set);
+//         return findMe(root,k,set);
+//     }
+    
+    // private boolean findMe(TreeNode root,int k,Set<Integer> set){
+    //     if(root == null) return false;
+    //     if(set.contains(k-root.val)) return true;
+    //     set.add(root.val);
+    //     return findMe(root.left,k,set)||findMe(root.right,k,set);
+    // }
+    
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> set = new ArrayList<>();
+        inorder(root,set);
+        int i=0;
+        int j= set.size()-1;
+        while(i<j){
+            if(set.get(i)+set.get(j)==k) return true;
+            else if(set.get(i)+set.get(j)<k) i+=1;
+            else j-=1;
+        }
+        return false;
     }
     
-    private boolean findMe(TreeNode root,int k,Set<Integer> set){
-        if(root == null) return false;
-        if(set.contains(k-root.val)) return true;
-        set.add(root.val);
-        return findMe(root.left,k,set)||findMe(root.right,k,set);
+    private void inorder(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
     }
 }
