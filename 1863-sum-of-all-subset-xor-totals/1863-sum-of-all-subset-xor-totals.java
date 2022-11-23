fb14c3395637c192741c5eec580ab92d47e47caa
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-23 20:01:12 +0530

    Time: 1 ms (85.97%), Space: 41.2 MB (63.35%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,27 +1,27 @@
 class Solution {
-    // public int subsetXORSum(int[] nums) {
-    //     return genSubsets(0,0,nums.length,nums);
-    // }
-    
-    // public int genSubsets(int ans,int i,int n,int a[]){
-    //     if(i==n)
-    //         return ans;
-    //     int x = genSubsets(ans^a[i],i+1,n,a);
-    //     int b = genSubsets(ans,i+1,n,a);
-    //     return x + b;
-    // }
     public int subsetXORSum(int[] nums) {
-        int ans =0;
-        int n = nums.length;
-        for(int i=0;i<(1<<n);i+=1){
-            int c =0;
-            for(int j=0;j<n;j+=1){
-                if((i&(1<<j))!=0){
-                    c^=nums[j];
-                }
-            }
-            ans+=c;
-        }
-        return ans;
+        return genSubsets(0,0,nums.length,nums);
     }
+    
+    public int genSubsets(int ans,int i,int n,int a[]){
+        if(i==n)
+            return ans;
+        int x = genSubsets(ans^a[i],i+1,n,a);
+        int b = genSubsets(ans,i+1,n,a);
+        return x + b;
+    }
+    // public int subsetXORSum(int[] nums) {
+    //     int ans =0;
+    //     int n = nums.length;
+    //     for(int i=0;i<(1<<n);i+=1){
+    //         int c =0;
+    //         for(int j=0;j<n;j+=1){
+    //             if((i&(1<<j))!=0){
+    //                 c^=nums[j];
+    //             }
+    //         }
+    //         ans+=c;
+    //     }
+    //     return ans;
+    // }
 }
