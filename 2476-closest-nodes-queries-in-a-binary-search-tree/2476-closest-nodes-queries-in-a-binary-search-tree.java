9461bafd56462056e18490107bc54991e12ad7e9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-11-25 17:59:25 +0530

    Time: 121 ms (90.82%), Space: 247.1 MB (5.01%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.java
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.java
@@ -86,30 +86,118 @@
 //         return ans;
 //     }
 // 	}
+// class Solution {
+//     TreeMap<Integer, Integer> map = new TreeMap<>();
+
+//     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         helper(root);
+//         for (int num : queries) {
+// 		//if num is present add num else add lower value than num
+//             Integer low = map.containsKey(num) ? Integer.valueOf(num) : map.lowerKey(num);
+// 		//if num is present add num else add higher value than num
+//             Integer high = map.containsKey(num) ? Integer.valueOf(num) : map.higherKey(num);
+// 		//if low or high value also not present add -1 else add low or high
+//             ans.add(Arrays.asList(low == null ? -1 : low, high == null ? -1 : high));
+//         }
+//         return ans;
+//     }
+
+//     void helper(TreeNode root) {
+//         if (root == null) return;
+//         map.put(root.val, 0);
+//         helper(root.left);
+//         helper(root.right);
+//     }
+
+// }
+// class Solution {
+//     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for(Integer i: queries){
+//             ans.add(search(root,i));
+//         }
+//         return ans;
+//     }
+    
+//     private List<Integer> search(TreeNode root, Integer i){
+//         int lower = Integer.MIN_VALUE;
+//         int upper = Integer.MAX_VALUE;
+//         while(root != null){
+//             if(root.val>i){
+//                 upper = Math.min(upper,root.val);
+//                 root= root.left;
+//             }else if(root.val<i){
+//                 lower = Math.max(lower,root.val);
+//                 root= root.right;
+//             }
+//             else{
+//                 return Arrays.asList(root.val,root.val);
+//             }
+            
+//         }
+//         if(lower==Integer.MIN_VALUE){
+//             lower = -1;
+//         }
+//         if(upper==Integer.MAX_VALUE){
+//             upper = -1;
+//         }
+//         return Arrays.asList(lower,upper);
+//     }
+// }
 class Solution {
-    TreeMap<Integer, Integer> map = new TreeMap<>();
-
-    public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(root);
-        for (int num : queries) {
-		//if num is present add num else add lower value than num
-            Integer low = map.containsKey(num) ? Integer.valueOf(num) : map.lowerKey(num);
-		//if num is present add num else add higher value than num
-            Integer high = map.containsKey(num) ? Integer.valueOf(num) : map.higherKey(num);
-		//if low or high value also not present add -1 else add low or high
-            ans.add(Arrays.asList(low == null ? -1 : low, high == null ? -1 : high));
-        }
-        return ans;
-    }
-
-    void helper(TreeNode root) {
+    List<Integer> nodes = new ArrayList<>(); 
+    void traverse(TreeNode root) {
         if (root == null) return;
-        map.put(root.val, 0);
-        helper(root.left);
-        helper(root.right);
+        
+        traverse(root.left);
+        nodes.add(root.val);
+        traverse(root.right);
     }
-//The `higherKey()`method class is used to return the least key strictly greater than the given key, or null if there is no such key.
-//AND
-//The `lowerKey()` method is used to return the greatest key strictly less than to given key, 
+    
+    List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+        traverse(root);                         // traverse all nodes and store them in ArrayList
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nodes.size();
+        
+        for (int q : queries) {            
+            if (q == nodes.get(0) || q == nodes.get(n-1)) {           // if value is already found, no need to do BS
+            	ans.add(Arrays.asList(q, q));
+            }
+            else if (q < nodes.get(0)) {
+            	ans.add(Arrays.asList(-1, nodes.get(0)));             // if value == first element, no need to do BS
+            }
+            else if (q > nodes.get(n-1)) {
+            	ans.add(Arrays.asList(nodes.get(n-1), -1));          // if value == last element, no need to do BS
+            }
+            else {
+            	int lo = 0;
+            	int hi = n-1;
+            	boolean found = false;
+            	while (lo < hi) {
+            		int m = (lo+hi) >> 1;
+            		int mid = nodes.get(m);
+            		if (q == mid) {
+            			found = true;
+            			ans.add(Arrays.asList(q, q));
+            			break;
+            		}
+            		else if (q > mid) {
+            			lo = m + 1;
+            		}
+            		else {
+            			hi = m;
+            		}
+            	}
+            	
+				// now lo and hi are equal
+            	if (!found) {
+            		ans.add(Arrays.asList(nodes.get(lo-1), nodes.get(lo)));
+            	}
+            }
+        }
+        
+        return ans;
+    } 
 }
