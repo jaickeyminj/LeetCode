b21488333822df3bbd114f348d8e6aea7693627b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-04 09:36:44 +0530

    Time: 4 ms (60.00%), Space: 42.3 MB (100.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,15 +1,32 @@
+// class Solution {
+//     public boolean isCircularSentence(String sentence) {
+//         String a[] = sentence.split("\\s+");
+//         if(a[0].charAt(0)!= a[a.length-1].charAt(a[a.length-1].length()-1))
+//             return false;
+//         char x='a';
+//         char y ='d';
+//         for(int i=0;i<a.length-1;i+=1){
+//             x= a[i].charAt(a[i].length()-1);
+//             y = a[i+1].charAt(0);
+//             if(x!=y) return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
-    public boolean isCircularSentence(String sentence) {
-        String a[] = sentence.split("\\s+");
-        if(a[0].charAt(0)!= a[a.length-1].charAt(a[a.length-1].length()-1))
-            return false;
-        char x='a';
-        char y ='d';
-        for(int i=0;i<a.length-1;i+=1){
-            x= a[i].charAt(a[i].length()-1);
-            y = a[i+1].charAt(0);
-            if(x!=y) return false;
+    public boolean isCircularSentence(String s) {
+        s=s.trim();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+                if(s.charAt(i-1)!=s.charAt(i+1))
+                {
+                    return false;
+                }
+            }
         }
-        return true;
+        return s.charAt(0) == s.charAt(s.length()-1);
     }
 }
