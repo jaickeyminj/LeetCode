a4a9fcaaa46838c6db8dc71204cbb11d1cadc8dc
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-04 10:42:21 +0530

    Time: 15 ms (100.00%), Space: 50.2 MB (85.71%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -1,14 +1,28 @@
 class Solution {
+    // public long dividePlayers(int[] skill) {
+    //     Arrays.sort(skill);
+    //     int ans = skill[0]+ skill[skill.length-1];
+    //     long res =0;
+    //     for(int i=0;i<skill.length/2;i+=1){
+    //         if(ans != (skill[i]+skill[skill.length-i-1])){
+    //             return -1;
+    //         }
+    //         res += (skill[i]*skill[skill.length-i-1]);
+    //     }
+    //     return res;
+    // }
     public long dividePlayers(int[] skill) {
         Arrays.sort(skill);
-        int ans = skill[0]+ skill[skill.length-1];
-        long res =0;
-        for(int i=0;i<skill.length/2;i+=1){
-            if(ans!= (skill[i]+skill[skill.length-i-1])){
-                return -1;
-            }
-            res += (skill[i]*skill[skill.length-i-1]);
+        int l = 0, r = skill.length - 1;
+        long ans = 0, set = skill[l] + skill[r];
+        while ( l < r ){
+            long diff = skill[l] + skill[r];
+            if ( set != diff ) return -1;
+            ans += skill[l] * skill[r];
+            l+=1; 
+            r-=1;
         }
-        return res;
+        return ans;
     }
+
 }
