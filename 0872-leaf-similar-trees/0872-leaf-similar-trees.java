0a61bd19dda4e3719eba33f71de87cdb9af07b20
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-08 10:45:45 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (84.91%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -14,20 +14,36 @@
  * }
  */
 class Solution {
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> leaves1 = new ArrayList();
-        List<Integer> leaves2 = new ArrayList();
-        dfs(root1, leaves1);
-        dfs(root2, leaves2);
-        return leaves1.equals(leaves2);
+//     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+//         List<Integer> leaves1 = new ArrayList();
+//         List<Integer> leaves2 = new ArrayList();
+//         dfs(root1, leaves1);
+//         dfs(root2, leaves2);
+//         return leaves1.equals(leaves2);
+//     }
+
+//     public void dfs(TreeNode node, List<Integer> leafValues) {
+//         if (node != null) {
+//             if (node.left == null && node.right == null)
+//                 leafValues.add(node.val);
+//             dfs(node.left, leafValues);
+//             dfs(node.right, leafValues);
+//         }
+//     }
+     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
+        s1.push(root1); s2.push(root2);
+        while (!s1.empty() && !s2.empty())
+            if (dfs(s1) != dfs(s2)) return false;
+        return s1.empty() && s2.empty();
     }
 
-    public void dfs(TreeNode node, List<Integer> leafValues) {
-        if (node != null) {
-            if (node.left == null && node.right == null)
-                leafValues.add(node.val);
-            dfs(node.left, leafValues);
-            dfs(node.right, leafValues);
+    public int dfs(Stack<TreeNode> s) {
+        while (true) {
+            TreeNode node = s.pop();
+            if (node.right != null) s.push(node.right);
+            if (node.left != null) s.push(node.left);
+            if (node.left == null && node.right == null) return node.val;
         }
     }
 }
