65bc8546ccb2c5b84dd54ab8931a72de63ca9ee6
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-08 15:39:40 +0530

    Time: 1 ms (100.00%), Space: 102.6 MB (31.56%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -9,20 +9,40 @@
  */
 
 class Solution {
-    TreeNode ans, target;
+//     TreeNode ans, target;
     
-    public void inorder(TreeNode o, TreeNode c) {
-        if (o == null) return;
-        inorder(o.left, c.left);
-        if (o == target) {
-                ans = c;    
-        }
-        inorder(o.right, c.right);    
-    }
+//     public void inorder(TreeNode o, TreeNode c) {
+//         if (o == null) return;
+//         inorder(o.left, c.left);
+//         if (o == target) {
+//                 ans = c;    
+//         }
+//         inorder(o.right, c.right);    
+//     }
     
+//     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+//         this.target = target;
+//         inorder(original, cloned);
+//         return ans;
+//     }
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        this.target = target;
-        inorder(original, cloned);
-        return ans;
+
+        if( original == null ){
+            return null;
+        }
+        
+        if( original == target ){
+            return cloned;
+        }
+        
+        TreeNode left =  getTargetCopy(original.left, cloned.left, target);
+        if( left != null ){ 
+            return left; 
+        
+        }else{
+            return getTargetCopy(original.right, cloned.right, target);
+        }        
+        
+        
     }
 }
