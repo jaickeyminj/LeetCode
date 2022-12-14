73e290508b791126dc722342cb551256644553bf
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-14 09:56:28 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (84.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,9 +5,10 @@ class Solution {
         int[] dp = {nums[0], Math.max(nums[0], nums[1])};
         int index=0;
         for(int i=2; i<nums.length; i++){
-            index = i&1; // i&1 === i%2.  //index^1: 0->1, 1->0.
+            index = i&1; // i&1 === i%2.  //index^1: 0->1, 1->0.//even
         	dp[index] = Math.max(nums[i]+dp[index], dp[index^1]); 
         }
+        // System.out.println(Arrays.toString(dp));
 		return Math.max(dp[0], dp[1]);
 	}
 }
