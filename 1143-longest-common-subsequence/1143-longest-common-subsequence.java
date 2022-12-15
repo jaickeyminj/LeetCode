d5f3b7056fb2e3c275da278454e9c901b14154f9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-15 09:35:11 +0530

    Time: 6 ms (99.32%), Space: 45.9 MB (84.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,27 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-//         char a[] = text1.toCharArray();
-//         char b[] = text1.toCharArray();
+        char a[] = text1.toCharArray();
+        char b[] = text2.toCharArray();
         
-//         int mat[][] = new int[a.length+1][b.length+1];
-//         int max = 0;
+        int mat[][] = new int[a.length+1][b.length+1];
+        int max = 0;
         
-//         for(int i=1;i<mat.length;i+=1){
-//             for(int j=1;j<mat[i].length;j+=1){
-//                 if(a[i-1]==b[j-1]){
-//                     mat[i][j]= mat[i-1][j-1]+1;
-//                 }
-//                 else{
-//                     mat[i][j]= Math.max(mat[i-1][j],mat[i][j-1]);
-//                 }
-//                 if(max<mat[i][j]){
-//                     max = mat[i][j];
-//                 }
-//             }
-//         }
-//         System.out.println(Arrays.deepToString(mat));
-//         return max;
-        return lcsDynamic(text1.toCharArray(),text2.toCharArray());
+        for(int i=1;i<mat.length;i+=1){
+            for(int j=1;j<mat[i].length;j+=1){
+                if(a[i-1] == b[j-1]){
+                    mat[i][j] = mat[i-1][j-1]+1;
+                }
+                else{
+                    mat[i][j]= Math.max(mat[i-1][j],mat[i][j-1]);
+                }
+                if(max<mat[i][j]){
+                    max = mat[i][j];
+                }
+            }
+        }
+        // System.out.println(Arrays.deepToString(mat));
+        return max;
+        // return lcsDynamic(text1.toCharArray(),text2.toCharArray());
     }
     public int lcsDynamic(char str1[],char str2[]){
     
