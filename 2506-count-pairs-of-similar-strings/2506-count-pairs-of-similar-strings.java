7acb0b2526af5602f5b05dcb50bd51531f0331b1
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-18 10:09:16 +0530

    Time: 95 ms (50.00%), Space: 68.6 MB (12.50%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
@@ -3,17 +3,20 @@ class Solution {
         int count = 0;
         
         for(int i=0;i<words.length-1;i+=1){
+            int map1[]= new int[26];
+            String a = words[i];
+            
+            for(char c: a.toCharArray()){
+                map1[c-'a']+=1;
+            }
             for(int j=i+1;j<words.length;j+=1){
                 // Arrays.fill(map1,0);
                 // Arrays.fill(map2,0);
-                int map1[]= new int[26];
+                
                 int map2[]= new int[26];
-                String a = words[i];
+                
                 String b = words[j];
                 
-                for(char c: a.toCharArray()){
-                    map1[c-'a']+=1;
-                }
                 for(char c: b.toCharArray()){
                     map2[c-'a']+=1;
                 }
