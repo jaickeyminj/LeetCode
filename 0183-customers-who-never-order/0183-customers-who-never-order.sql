a559d5d6983d6a867567c12015b41babcd5865ec
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2022-12-29 11:20:54 +0530

    Time: 822 ms (25.13%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-# select c.name as Customers from Customers c LEFT JOIN Orders o on c.id = o.customerId where o.CustomerId IS NULL;
-SELECT Name AS Customers FROM Customers WHERE Id Not In (SELECT CustomerID FROM Orders)
+select c.name as Customers from Customers c LEFT JOIN Orders o on c.id = o.customerId where o.CustomerId IS NULL;
+# SELECT Name AS Customers FROM Customers WHERE Id Not In (SELECT CustomerID FROM Orders)
