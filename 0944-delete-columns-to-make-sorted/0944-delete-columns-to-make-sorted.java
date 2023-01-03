a43b8876cafbf9186c48830549aa80e15c43e5c0
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-03 15:33:15 +0530

    Time: 15 ms (50.56%), Space: 48.4 MB (13.71%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -4,13 +4,13 @@ class Solution {
         final int row = strs.length;
         int count =0;
         for(int i=0;i<cols;i+=1){
-            char c = strs[0].charAt(i);
+            // char c = strs[0].charAt(i);
             for(int j=1;j<row;j+=1){
-                if(strs[j].charAt(i)<c){
+                if(strs[j].charAt(i)<strs[j-1].charAt(i)){
                     count+=1;
                     break;
                 }
-                c = strs[j].charAt(i);
+                // c = strs[j].charAt(i);
             }
         }
         return count;
