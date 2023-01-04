606c9ffca3c3057f4a39bb2082e53d2ebca86994
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-04 15:08:52 +0530

    Time: 15 ms (10.02%), Space: 42.8 MB (11.10%) - LeetHub

---
+++ 2451-odd-string-difference/2451-odd-string-difference.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String oddString(String[] words) {
+        // Map<Integer[],List<String>> map = new HashMap<>();
+        // Arrays.sort(words);
+        int row = words.length;
+        int cols = words[0].length();
+        Integer x[][]=  new Integer[row][cols-1];
+        // Arrays.fill(x,100);
+        for(int i=0;i<row;i+=1){
+            for(int j=0;j<cols-1;j+=1){
+                x[i][j] =findChar(words[i].charAt(j+1))-findChar(words[i].charAt(j));
+                
+            }
+        }
+        System.out.println(Arrays.deepToString(x));
+        for(int i=1;i<row-1;i+=1){
+            for(int j=0;j<cols-1;j+=1){
+            if(x[i][j]==x[i+1][j] && x[i-1][j]!=x[i][j])
+                return words[i-1];
+            if(x[i-1][j]==x[i+1][j] && x[i+1][j]!=x[i][j])
+                return words[i];
+            if(x[i][j]==x[i-1][j] && x[i+1][j]!=x[i][j])
+                return words[i+1];
+            }
+        }
+        return ""+words[words.length-1];
+    }
+    
+    public int findChar(char a){
+        return a-'a';
+    }
+}
