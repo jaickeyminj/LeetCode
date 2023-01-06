0bd6eef94436425189a9a6ab0347f3eece9ab935
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-06 12:17:33 +0530

    Time: 52 ms (40.52%), Space: 77.9 MB (20.38%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,15 +1,26 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
-        for(int i:costs)
-            pq.offer(i);
-        // System.out.println(pq);
-        int max =0;
-        while(!pq.isEmpty()&& coins>0){
-            int poll = pq.poll();
-            max += coins-poll>=0?1:0; 
-            coins-=poll;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
+        // for(int i:costs)
+        //     pq.offer(i);
+        // int max =0;
+        // while(!pq.isEmpty()&& coins>0){
+        //     int poll = pq.poll();
+        //     max += coins-poll>=0?1:0; 
+        //     coins-=poll;
+        // }
+        // return max;
+        Arrays.sort(costs);
+        int max = costs.length;
+        for(int i:costs){
+            if(coins-i>=0){
+                coins-=i;
+                max-=1;
+            }
+            else{
+                break;
+            }
         }
-        return max;
+        return costs.length-max;
     }
 }
