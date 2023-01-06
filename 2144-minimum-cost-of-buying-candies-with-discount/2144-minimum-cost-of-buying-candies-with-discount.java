dbc6593c00ca9da07b00189f84d51b2fd3e16a10
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-06 17:43:05 +0530

    Time: 4 ms (62.07%), Space: 42.4 MB (57.76%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,20 +1,30 @@
 class Solution {
+    // public int minimumCost(int[] cost) {
+    //     final int len = cost.length;
+    //     if(len==1)
+    //         return cost[0];
+    //     if(len==2)
+    //         return cost[0]+cost[1];
+    //     else{
+    //         int sum =0;
+    //         Arrays.sort(cost);
+    //         int i = len-1;
+    //         for(;i>=1;i-=3){
+    //             sum += cost[i]+cost[i-1];
+    //         }
+    //         if(i==0)
+    //             return sum+cost[0];
+    //         return sum;
+    //     }
+    // }
     public int minimumCost(int[] cost) {
-        final int len = cost.length;
-        if(len==1)
-            return cost[0];
-        if(len==2)
-            return cost[0]+cost[1];
-        else{
-            int sum =0;
-            Arrays.sort(cost);
-            int i = len-1;
-            for(;i>=1;i-=3){
-                sum += cost[i]+cost[i-1];
-            }
-            if(i==0)
-                return sum+cost[0];
-            return sum;
+        Arrays.sort(cost);
+        int n = cost.length;
+        int sum=0;
+        for(int i=0;i<n;i+=1){
+            if(i%3!=n%3)
+                sum+=cost[i];
         }
+        return sum;
     }
 }
