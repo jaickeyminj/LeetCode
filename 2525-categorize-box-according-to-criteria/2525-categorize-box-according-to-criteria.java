9e8ea5d4ed24fc58aef7699137a79b8340130b2c
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-08 22:03:02 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (100.00%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String categorizeBox(int length, int width, int height, int mass) {
+        if(isBulky(length,width,height) && mass >= 100)     return "Both";
+        else if(!(isBulky(length,width,height)) && mass < 100)      return "Neither";
+        else if(!(isBulky(length,width,height)) && mass >= 100)     return "Heavy";
+        else    return "Bulky";
+    
+    }
+    private static boolean isBulky(int length, int width, int height){
+        long volume = (long)length * (long)width * (long)height;
+
+        if(Math.max(length,Math.max(width,height)) >= 10_000)   return true;
+        if( volume >= 1000_000_000) return true;
+        return false;
+    }
+}
