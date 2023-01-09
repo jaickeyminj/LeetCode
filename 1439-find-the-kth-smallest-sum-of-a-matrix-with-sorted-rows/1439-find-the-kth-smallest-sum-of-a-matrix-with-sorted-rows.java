26b19440ea29da17bf46722b970ce97143824878
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-09 14:48:29 +0530

    Time: 260 ms (43.92%), Space: 42.1 MB (89.72%) - LeetHub

--- 1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.java
+++ 1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.java
@@ -3,19 +3,19 @@ class Solution {
         int col = Math.min(mat[0].length, k);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        PriorityQueue<Integer> nextPq;
         pq.add(0);
         for (int[] row : mat) {
-            // max priority queue for the i-th row
-            PriorityQueue<Integer> nextPq = new PriorityQueue<>((a,b)->b-a);
-            for (int i : pq) {
+             nextPq = new PriorityQueue<>((a,b)->b-a);
+            
                 for (int c = 0; c < col; c++) {
-                    nextPq.add(i + row[c]);
-                    // keep pq size <= k
-                    if (nextPq.size() > k) {
-                        nextPq.poll();
+                    for (int i : pq) {
+                        nextPq.add(i + row[c]);
+                        if (nextPq.size() > k) {
+                            nextPq.poll();
+                        }
                     }
                 }
-            }
             pq = nextPq;
         }
         return pq.poll();
