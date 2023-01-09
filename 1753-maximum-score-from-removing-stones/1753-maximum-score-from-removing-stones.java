a2602da8c686db64ba27914bdbd70fe06b47845b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-09 11:55:20 +0530

    Time: 88 ms (48.75%), Space: 41.7 MB (56.63%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.java
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.java
@@ -12,9 +12,9 @@ class Solution {
             i-=1;
             j-=1;
             ans+=1;
-            if(i>0)
+            if(i>=1)
                 q.offer(i);
-            if(j>0)
+            if(j>=1)
                 q.offer(j);
         }
         
