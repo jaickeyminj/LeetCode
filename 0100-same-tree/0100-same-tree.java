504cea3343be24aea4b4a61a234226ee3593ed45
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-10 18:50:10 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (62.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -4,34 +4,17 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
-        helper(p,a);
-        helper(q,b);
-        // System.out.println(a+"\n"+b);
-        // return a.toString().contentEquals(b.toString());
-        return a.equals(b);
-    }
-    
-    public void helper(TreeNode p,List<Integer> a){
-        if(p!=null){
-            a.add(p.val);
-            helper(p.left,a);
-            helper(p.right,a);
-        }else{
-            a.add(null);
-            return;
-        }
-    }
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    // p and q are both null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.right, q.right) &&
+            isSameTree(p.left, q.left);
+  }
 }
