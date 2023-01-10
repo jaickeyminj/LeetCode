014498decdcb79d33a26c89bc561e0ec7f7bde7d
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-10 18:58:47 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (83.22%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,39 +14,56 @@
  * }
  */
 class Solution {
-    public int sumEvenGrandparent(TreeNode root) {
-        int sum = 0;
-        Queue<TreeNode> q = new LinkedList();
-        q.add(root);
-        
-        //LevelOrderTraversal
-        while(!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if(node.left != null) {
-                q.add(node.left);
-                if(node.val % 2 == 0) {
-                    if(node.left.left != null) {
-                        sum += node.left.left.val;
-                    }
-                    if(node.left.right != null) {
-                        sum += node.left.right.val;
-                    }
-                }
-            }
+    public static int sum = 0;
 
-            if(node.right != null) {
-                q.add(node.right);
-                if(node.val % 2 == 0) {
-                    if(node.right.left != null) {
-                        sum += node.right.left.val;
-                    }
-                    if(node.right.right != null) {
-                        sum += node.right.right.val;
-                    }
-                }
-            }
-        }
-        
+    public int sumEvenGrandparent(TreeNode root) {
+        sum=0;
+        dfs(root, null, null);
         return sum;
     }
+
+    private void dfs(TreeNode current, TreeNode parent, TreeNode grandParent) {
+        if (current == null) return; // base case 
+        if (grandParent != null && grandParent.val % 2 == 0) {
+            sum += current.val;
+        }
+        dfs(current.left, current, parent);// ( newChild, parent, GrandParent)
+        dfs(current.right, current, parent);
+    }
+    
+    // public int sumEvenGrandparent(TreeNode root) {
+//         int sum = 0;
+//         Queue<TreeNode> q = new LinkedList();
+//         q.add(root);
+        
+//         //LevelOrderTraversal
+//         while(!q.isEmpty()) {
+//             TreeNode node = q.poll();
+//             if(node.left != null) {
+//                 q.add(node.left);
+//                 if(node.val % 2 == 0) {
+//                     if(node.left.left != null) {
+//                         sum += node.left.left.val;
+//                     }
+//                     if(node.left.right != null) {
+//                         sum += node.left.right.val;
+//                     }
+//                 }
+//             }
+
+//             if(node.right != null) {
+//                 q.add(node.right);
+//                 if(node.val % 2 == 0) {
+//                     if(node.right.left != null) {
+//                         sum += node.right.left.val;
+//                     }
+//                     if(node.right.right != null) {
+//                         sum += node.right.right.val;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return sum;
+//     }
 }
