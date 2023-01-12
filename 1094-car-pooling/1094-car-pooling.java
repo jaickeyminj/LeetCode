baba7d4f7b51fe183a6e0650f147dcbf3a894c30
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-12 11:13:04 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (98.03%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,16 +1,30 @@
 class Solution {
-    public boolean carPooling(int[][] trips, int capacity) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
-            return Integer.compare(a[0], b[0]);
-        });
+//     public boolean carPooling(int[][] trips, int capacity) {
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+//             if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
+//             return Integer.compare(a[0], b[0]);
+//         });
 
-        for(int[] trip : trips){
-            pq.offer(new int[]{trip[1], trip[0]});
-            pq.offer(new int[]{trip[2], -trip[0]});
+//         for(int[] trip : trips){
+//             pq.offer(new int[]{trip[1], trip[0]});
+//             pq.offer(new int[]{trip[2], -trip[0]});
+//         }
+//         while(!pq.isEmpty()){
+//             capacity -= pq.poll()[1];
+//             if(capacity < 0) return false;
+//         }
+//         return true;
+//     }
+    public boolean carPooling(int[][] trips, int capacity) {
+        int lengthOfTrip[] = new int[1001];
+        
+        for(int trip[] : trips){
+            lengthOfTrip[trip[1]] += trip[0]; 
+            lengthOfTrip[trip[2]] -= trip[0]; 
         }
-        while(!pq.isEmpty()){
-            capacity -= pq.poll()[1];
+        
+        for(int passenger : lengthOfTrip){
+            capacity -= passenger;
             if(capacity < 0) return false;
         }
         return true;
