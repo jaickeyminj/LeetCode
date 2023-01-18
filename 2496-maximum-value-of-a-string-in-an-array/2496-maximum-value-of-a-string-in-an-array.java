ec283dea08530a2b35fcfea567f725d82254b9fb
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-18 18:07:20 +0530

    Time: 1 ms (92.36%), Space: 39.8 MB (89.06%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
@@ -1,19 +1,35 @@
 import java.util.regex.Pattern;
 class Solution {
+    // public int maximumValue(String[] strs) {
+    //     Set<Integer> set = new TreeSet<>();
+    //     Pattern pattern = Pattern.compile(".*[^0-9].*");
+    //     for(String s: strs){
+    //         if(!pattern.matcher(s).matches()){
+    //             set.add(Integer.parseInt(s));
+    //         }
+    //         else{
+    //             set.add(s.length());
+    //         }
+    //     }
+    //     int res =0;
+    //     for(Integer i:set)
+    //         res = i;
+    //     return res;
+    // }
     public int maximumValue(String[] strs) {
-        Set<Integer> set = new TreeSet<>();
-        Pattern pattern = Pattern.compile(".*[^0-9].*");
-        for(String s: strs){
-            if(!pattern.matcher(s).matches()){
-                set.add(Integer.parseInt(s));
+        int i,j,maxValue=0;
+        for(i=0;i<strs.length;i++){
+            j=0;
+            while(j< strs[i].length() && 
+                  (strs[i].charAt(j) >= '0' && strs[i].charAt(j) <= '9')){
+                j++;
             }
-            else{
-                set.add(s.length());
+            if(strs[i].length()==j){
+                maxValue = Math.max(maxValue,Integer.parseInt(strs[i]));
+            }else{
+                maxValue= Math.max(maxValue,strs[i].length());
             }
-        }
-        int res =0;
-        for(Integer i:set)
-            res = i;
-        return res;
+        }        
+        return maxValue;
     }
 }
