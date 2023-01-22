c2504625aa1d86b68afd9bfe7a26e49b59efdd6e
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-23 00:49:48 +0530

    Time: 8 ms (72.33%), Space: 43 MB (70.72%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,36 +1,14 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        // Map<String,Integer> map = new HashMap<>();
-        // // Map<Integer,String> map2 = new HashMap<>();
-        // for(int i=0;i<names.length;i+=1){
-        //     map.put(names[i],heights[i]);
-        //     // map2.put(heights[i],names[i]);
-        // }
-        // System.out.println(map);
-        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-        //     return map.get(b)-map.get(a);
-        // });
-        // for(int h:heights)
-        //     pq.offer(h);
-        // int i=0;
-        // while(!pq.isEmpty()){
-        //     int j = (pq.poll());
-        //     for(String s: map.keySet())
-        //         if(map.get(s)==j){
-        //             names[i++]=s;
-        //             break;
-        //         }
-        // }
-        // return names;
-        String res[] = new String[names.length];
-        int ind = names.length;
-        Map<Integer, String> map = new TreeMap<>();
-        for(int i = 0;i<names.length;i++){
-            map.put(heights[i], names[i]);
+        PriorityQueue<Pair<String,Integer>>pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        for(int i=0;i<names.length;i++){
+            pq.add(new Pair<>(names[i],heights[i]));
         }
-        for(String p : map.values()){
-            res[--ind] = p;
+        int i=0;
+        String ans[]=new String[names.length];
+        while(!pq.isEmpty()){
+          ans[i++]=pq.poll().getKey();  
         }
-        return res;
+        return ans;
     }
 }
