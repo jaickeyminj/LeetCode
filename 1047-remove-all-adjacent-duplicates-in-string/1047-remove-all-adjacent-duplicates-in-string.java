f315de5119cf667bfeaaefb8cddc2c14a95c1af3
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-23 13:24:31 +0530

    Time: 219 ms (24.98%), Space: 43.2 MB (59.44%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack <Character> stack= new Stack<>(); 
+        for(char ch: s.toCharArray()) {
+            stack.push(ch);
+            while (stack.size() > 1) {
+                    char top1= stack.pop();
+                    if (stack.peek() == top1) 
+                        stack.pop();
+                    else {
+                        stack.push(top1);
+                        break;
+                    }
+                }
+        
+        }
+        return stack.toString().replaceAll("(\\[|\\]|\\,|\\s+)", "");
+    }
+}
