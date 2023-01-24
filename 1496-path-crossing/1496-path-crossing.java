701312e451bab821eaa8f919fba11faf1153ced1
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-24 13:54:34 +0530

    Time: 8 ms (24.68%), Space: 42.7 MB (23.08%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        // int a[]= new int[]{0,0};
+        Set<String> set = new HashSet<>();
+        set.add(0+","+0);
+        int x =0;
+        int y =0;
+        for(char c: path.toCharArray()){
+            if(c=='N'){
+                x +=1;
+            }
+            else if(c=='S'){
+                x -=1;
+            }
+            else if(c=='W'){
+                y +=1;
+            }
+            else{
+                y -=1;
+            }
+            if(set.contains(x+","+y)){
+                return true;
+            }
+            else
+                set.add(x+","+y);
+        }
+        return false;
+    }
+}
