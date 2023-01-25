002b17077e6290ce769aa6064911d8768543d417
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-25 11:07:34 +0530

    Time: 27 ms (98.40%), Space: 81.1 MB (97.20%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        int m = operations.length;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i+=1){
-            map.put(nums[i],i);
+        int[] map = new int[1000001];
+        int i=0; 
+        for(int x:nums){
+            map[x]=i;
+            i+=1;
         }
-        for(int i=0;i<m;i+=1){
-            int pos=map.get(operations[i][0]);
-            nums[pos]=operations[i][1];
-            map.put(operations[i][1],pos);
+
+        for(int[] x : operations)
+        {
+            int idx = map[x[0]];
+            nums[idx]=x[1];
+            map[x[1]]=idx;
         }
         return nums;
     }
