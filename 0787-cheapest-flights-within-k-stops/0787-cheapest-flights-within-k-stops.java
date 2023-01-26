a255e4d991e058736eb5ba8238821813d31eb672
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-01-26 09:16:42 +0530

    Time: 1 ms (99.90%), Space: 46.7 MB (31.44%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,19 +1,28 @@
 class Solution {
-     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int []prices=new int[n];
-        Arrays.fill(prices,Integer.MAX_VALUE);
-        prices[src]=0;
-        for(int i=0;i<=k;i++)
-        {
-            int []temp=Arrays.copyOf(prices,n);
-            for(int[]flight:flights)
-            {
-                int cur=flight[0],next=flight[1],price=flight[2];
-                if(prices[cur]==Integer.MAX_VALUE)continue;
-                temp[next]=Math.min(temp[next],prices[cur]+price);
-            }
-            prices=temp;
-        }
-        return prices[dst]==Integer.MAX_VALUE?-1:prices[dst];
-    }
+	public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+		int[] dp = new int[n];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[src] = 0;
+		while (k-- >= 0) {
+			if (this.isExistsRoute(flights, dp)) {
+				break;
+			}
+		}
+		return dp[dst] == Integer.MAX_VALUE ? -1 : dp[dst];
+	}
+
+	private boolean isExistsRoute(int[][] flights, int[] dp) {
+		int[] temp = Arrays.copyOf(dp, dp.length);
+		boolean result = true;
+		for (int[] flight : flights) {
+			int src = flight[0];
+			int dst = flight[1];
+			int cost = flight[2];
+			if (temp[src] < Integer.MAX_VALUE && dp[dst] > dp[src] + cost) {
+				dp[dst] = temp[src] + cost;
+				result = false;
+			}
+		}
+		return result;
+	}
 }
