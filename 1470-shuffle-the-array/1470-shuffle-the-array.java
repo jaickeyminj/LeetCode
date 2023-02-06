03d731c2ff7acf20711becc034f3af333a5fa0d9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-02-06 17:14:58 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (90.42%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[]= new int[nums.length];
+        
+        for(int i=0;i<ans.length/2;i++){
+            ans[i*2]= nums[i];
+            // System.out.println(i+n);
+            ans[i*2+1]=nums[i+n];
+        }
+        return ans;
+    }
+}
