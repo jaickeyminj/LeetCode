ec25fcc08f7cff27f24fda3fe2d1ec0949061271
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-02-21 11:41:16 +0530

    Time: 0 ms (100.00%), Space: 48.4 MB (39.47%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,15 +1,27 @@
 class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         Map<Integer,Integer> map = new HashMap<>();
+        
+//         for(int i:nums){
+//             map.put(i,map.getOrDefault(i,0)+1);
+//         }
+        
+//         for(Map.Entry<Integer,Integer> m: map.entrySet()){
+//             if(m.getValue()==1)
+//                 return m.getKey();
+//         }
+//         return -1;
+//     }
     public int singleNonDuplicate(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        int left = 0, right = nums.length-1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if( (mid % 2 == 0 && nums[mid] == nums[mid +1]) ||
+               (mid %2 == 1 && nums[mid] == nums[mid - 1]) )
+                left = mid + 1;
+            else
+                right = mid;
         }
-        
-        for(Map.Entry<Integer,Integer> m: map.entrySet()){
-            if(m.getValue()==1)
-                return m.getKey();
-        }
-        return -1;
+        return nums[left];
     }
 }
