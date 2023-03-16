5bf7a781dab5124fe22a443c550603b3024561c9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-03-16 09:10:17 +0530

    Time: 13 ms (6.18%), Space: 88.5 MB (5.31%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length == 0 || postorder.length == 0) {
+            return null;
+        }
+
+        // The last element in postorder is the root of the tree
+        TreeNode root = new TreeNode(postorder[postorder.length-1]);
+
+        // Find the index of the root in inorder traversal
+        int inorderRootIdx = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == root.val) {
+                inorderRootIdx = i;
+                break;
+            }
+        }
+
+        // Recursively build the left and right subtrees
+        root.left = buildTree(Arrays.copyOfRange(inorder, 0, inorderRootIdx), 
+                              Arrays.copyOfRange(postorder, 0, inorderRootIdx));
+        root.right = buildTree(Arrays.copyOfRange(inorder, inorderRootIdx+1, inorder.length), 
+                               Arrays.copyOfRange(postorder, inorderRootIdx, postorder.length-1));
+
+        return root;
+    }
+}
