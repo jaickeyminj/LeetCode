1e2d67e76bd9900cde17506ea61b8d6899ee4240
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-03-16 09:12:39 +0530

    Time: 2 ms (97.15%), Space: 41.8 MB (82.96%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,28 +15,56 @@
  */
 class Solution {
     
+//     public TreeNode buildTree(int[] inorder, int[] postorder) {
+//         if (inorder.length == 0 || postorder.length == 0) {
+//             return null;
+//         }
+
+//         // The last element in postorder is the root of the tree
+//         TreeNode root = new TreeNode(postorder[postorder.length-1]);
+
+//         // Find the index of the root in inorder traversal
+//         int inorderRootIdx = 0;
+//         for (int i = 0; i < inorder.length; i++) {
+//             if (inorder[i] == root.val) {
+//                 inorderRootIdx = i;
+//                 break;
+//             }
+//         }
+
+//         // Recursively build the left and right subtrees
+//         root.left = buildTree(Arrays.copyOfRange(inorder, 0, inorderRootIdx), 
+//                               Arrays.copyOfRange(postorder, 0, inorderRootIdx));
+//         root.right = buildTree(Arrays.copyOfRange(inorder, inorderRootIdx+1, inorder.length), 
+//                                Arrays.copyOfRange(postorder, inorderRootIdx, postorder.length-1));
+
+//         return root;
+//     }
+    
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (inorder.length == 0 || postorder.length == 0) {
             return null;
         }
 
-        // The last element in postorder is the root of the tree
+        Stack<TreeNode> stack = new Stack<>();
         TreeNode root = new TreeNode(postorder[postorder.length-1]);
+        stack.push(root);
 
-        // Find the index of the root in inorder traversal
-        int inorderRootIdx = 0;
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == root.val) {
-                inorderRootIdx = i;
-                break;
+        int inorderIdx = inorder.length-1;
+        for (int i = postorder.length-2; i >= 0; i--) {
+            TreeNode curr = stack.peek();
+            if (curr.val != inorder[inorderIdx]) {
+                curr.right = new TreeNode(postorder[i]);
+                stack.push(curr.right);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIdx]) {
+                    curr = stack.pop();
+                    inorderIdx--;
+                }
+                curr.left = new TreeNode(postorder[i]);
+                stack.push(curr.left);
             }
         }
-
-        // Recursively build the left and right subtrees
-        root.left = buildTree(Arrays.copyOfRange(inorder, 0, inorderRootIdx), 
-                              Arrays.copyOfRange(postorder, 0, inorderRootIdx));
-        root.right = buildTree(Arrays.copyOfRange(inorder, inorderRootIdx+1, inorder.length), 
-                               Arrays.copyOfRange(postorder, inorderRootIdx, postorder.length-1));
 
         return root;
     }
