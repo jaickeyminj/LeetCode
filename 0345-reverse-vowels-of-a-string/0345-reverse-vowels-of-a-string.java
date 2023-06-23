ed6c5ac9be7314645fbf11d564ecf3f2e2950031
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-06-23 12:55:35 +0530

    Time: 6 ms (60.76%), Space: 43.6 MB (76.90%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,27 +1,44 @@
 class Solution {
-    public String reverseVowels(String s) {
-        Set<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');set.add('i');set.add('o');set.add('u');
-        set.add('A');set.add('E');set.add('I');set.add('O');set.add('U');
+//     public String reverseVowels(String s) {
+//         Set<Character> set = new HashSet<>();
+//         set.add('a');
+//         set.add('e');set.add('i');set.add('o');set.add('u');
+//         set.add('A');set.add('E');set.add('I');set.add('O');set.add('U');
         
-        Stack<Character> stack = new Stack<>();
-        StringBuilder sb,sb2;
-        // sb = new StringBuilder();
-        sb2 = new StringBuilder();
-        for(char c:s.toCharArray()){
-            if(set.contains(c)){
-                stack.push(c);
+//         Stack<Character> stack = new Stack<>();
+//         StringBuilder sb,sb2;
+//         sb2 = new StringBuilder();
+//         for(char c:s.toCharArray()){
+//             if(set.contains(c)){
+//                 stack.push(c);
+//             }
+//         }
+//         for(char c: s.toCharArray()){
+//             if(set.contains(c)){
+//                 sb2.append(stack.pop());
+//                 continue;
+//             }
+//             sb2.append(c);
+//         }
+//         return sb2.toString();
+//     }
+    public String reverseVowels(String s) {
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (!set.contains(arr[left])) {
+                left++;
+            } else if (!set.contains(arr[right])) {
+                right--;
+            } else {
+                char tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+                left++;
+                right--;
             }
-            // sb.append(c);
         }
-        for(char c: s.toCharArray()){
-            if(set.contains(c)){
-                sb2.append(stack.pop());
-                continue;
-            }
-            sb2.append(c);
-        }
-        return sb2.toString();
+        return new String(arr);
     }
 }
