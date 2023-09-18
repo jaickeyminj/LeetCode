689ed96c3777b1522bf713d23e521b5189e16fda
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-09-18 17:06:25 +0530

    Time: 522 ms (5.01%), Space: 51.3 MB (40.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,19 +5,19 @@ class Solution {
             sum+=i;
         }
         if(sum%2!=0) return false;
-        // Map<Integer,Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        // map.put(0,0);
-        set.add(0);
+        Map<Integer,Integer> map = new HashMap<>();
+        // Set<Integer> set = new HashSet<>();
+        map.put(0,0);
+        // set.add(0);
         for(int i: nums){
-            // Map<Integer,Integer> map2 = map;
-            Set<Integer> set2 = new HashSet<>(set);
-            for(Integer e : set2){
-                set.add(e+i);
+            Map<Integer,Integer> map2 = new HashMap<>(map);
+            // Set<Integer> set2 = new HashSet<>(set);
+            for(Integer e : map2.keySet()){
+                map.put(e+i,0);
                 
             }
         }
         // System.out.println(sum/2);
-        return set.contains(sum/2);
+        return map.containsKey(sum/2);
     }
 }
