43e197bd395cbdfbf1913348fe5bd2623de35399
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-09-28 12:07:53 +0530

    Time: 2 ms (68.44%), Space: 44.5 MB (55.51%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,21 +1,36 @@
 class Solution {
-    public int jump(int[] nums) {
-        // The starting range of the first jump is [0, 0]
-        int answer = 0, n = nums.length;
-        int curEnd = 0, curFar = 0;
+//     public int jump(int[] nums) {
+//         // The starting range of the first jump is [0, 0]
+//         int answer = 0, n = nums.length;
+//         int curEnd = 0, curFar = 0;
         
-        for (int i = 0; i < n - 1; ++i) {
-            // Update the farthest reachable index of this jump.
-            curFar = Math.max(curFar, i + nums[i]);
+//         for (int i = 0; i < n - 1; ++i) {
+//             // Update the farthest reachable index of this jump.
+//             curFar = Math.max(curFar, i + nums[i]);
 
-            // If we finish the starting range of this jump,
-            // Move on to the starting range of the next jump.
-            if (i == curEnd) {
-                answer++;
-                curEnd = curFar;
-            }
-        }
+//             // If we finish the starting range of this jump,
+//             // Move on to the starting range of the next jump.
+//             if (i == curEnd) {
+//                 answer++;
+//                 curEnd = curFar;
+//             }
+//         }
         
-        return answer;
+//         return answer;
+//     }
+    public int jump(int[] nums) {
+        int max = 0;
+        int end = -1;
+        int cnt = 0;
+        
+        for(int i=0;i<nums.length-1;i+=1){
+            if(i>end){
+                cnt+=1;
+                end = max;
+            }
+            max = Math.max(max,nums[i]+i);
+            if(max>=nums.length-1) return cnt;
+        }
+        return cnt;
     }
 }
