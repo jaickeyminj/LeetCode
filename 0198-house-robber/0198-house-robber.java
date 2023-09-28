0a09c1145080eaad2cfc2d13ceafba3d705903a6
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-09-28 10:19:07 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (92.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int rob(int[] nums) {
+    /*public int rob(int[] nums) {
         if(nums.length==0) return 0;
         if(nums.length==1) return nums[0];
         int[] dp = {nums[0], Math.max(nums[0], nums[1])};
@@ -10,5 +10,16 @@ class Solution {
         }
         // System.out.println(Arrays.toString(dp));
 		return Math.max(dp[0], dp[1]);
-	}
+	}*/
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1],nums[0]);
+        for(int i=2;i<nums.length;i+=1){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[dp.length-1];
+    }
 }
