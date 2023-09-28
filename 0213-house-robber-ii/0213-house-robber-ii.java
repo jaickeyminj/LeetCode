62d91ef53b8a439e8a5933b1c928cc29d79c7b97
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-09-28 10:46:32 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (76.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -4,18 +4,21 @@ class Solution {
         if(nums.length==0) return 0;
         if(nums.length==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
-        int start = 0;
-        int end = n-2;
+        
         int dp[] = new int[n];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0],nums[1]);
+        //leave last house
         for(int i=2;i<n-1;i+=1){
             dp[i] = Math.max(dp[(i-2)] + nums[i], dp[(i-1)]);
         }
         int max1 = dp[n-2];
+        
         Arrays.fill(dp,0);
+        
         dp[1] = nums[1];
         dp[2] = Math.max(nums[1],nums[2]);
+        //leave first house
         for(int i=3;i<n;i+=1){
             dp[i] = Math.max(dp[i-2]+ nums[i],dp[i-1]);
         }
