d40bfbc8947d5e85fef6897c7bd385461f301903
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-10-01 00:40:29 +0530

    Time: 20 ms (50.00%), Space: 56.4 MB (50.00%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -6,14 +6,15 @@ class Solution {
    		count.put(num, count.getOrDefault(num, 0) + 1);
    	}
 
-   	int result = 0;
-   	for (Integer value : count.values()) {
-   		if (value == 1) return -1;
-
-   		result += value / 3;
-   		if (value % 3 != 0) result+=1;
+   	int ans = 0;
+   	for (Integer v : count.values()) {
+        if(v==1) return -1;
+   		if(v%3 == 0)
+            ans += v/3;
+        else
+            ans += v/3 + 1;
    	}
 
-   	return result;
+   	return ans;
    }
 }
