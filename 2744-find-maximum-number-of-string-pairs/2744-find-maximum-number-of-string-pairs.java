21b174d11d3b0aa4f8d649d7fef87d1beeabfec1
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-10-11 19:31:54 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (99.56%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
@@ -1,31 +1,32 @@
+// class Solution {
+//     public int maximumNumberOfStringPairs(String[] words) {
+//         Map<String,Integer> map = new HashMap<>();
+//         for(String w: words){
+//             StringBuffer sb = new StringBuffer();
+//             sb.append(w);
+//             if(w.equals(sb.reverse().toString())){}
+//             else
+//                 map.put(w,0);
+//         }
+        
+//         int cnt = 0;
+//         for(String s: map.keySet()){
+//             StringBuffer sb = new StringBuffer();
+//             sb.append(s);
+//             if(map.containsKey(sb.reverse().toString()))
+//                 cnt+=1;
+//         }        
+//         return cnt/2;
+//     }
+// }
 class Solution {
     public int maximumNumberOfStringPairs(String[] words) {
-        Map<String,Integer> map = new HashMap<>();
-        for(String w: words){
-            StringBuffer sb = new StringBuffer();
-            sb.append(w);
-            if(w.equals(sb.reverse().toString())){}
-                
-            else
-                map.put(w,0);
+        int ans = 0;
+        for(int i = 0 ; i < words.length - 1; i++){
+            for(int j = i + 1 ; j < words.length ; j++){
+                if(words[i].charAt(0) == words[j].charAt(1) && words[i].charAt(1) == words[j].charAt(0))ans++;
+            }
         }
-        
-        int cnt = 0;
-        // System.out.println(map);
-        // System.out.println(map.containsKey("ca"));
-        for(String s: map.keySet()){
-            StringBuffer sb = new StringBuffer();
-            sb.append(s);
-            // System.out.println(s+" - "+(new StringBuffer().append(s).reverse()));
-//             if(s.equals(sb.reverse())){
-                
-//             }
-//             else //
-                if(map.containsKey(sb.reverse().toString()))//(sb.reverse().equals(s)))//{
-                    cnt+=1;
-            // System.out.println(s+" - "+(new StringBuffer().append(s).reverse()));}
-        }
-        
-        return cnt/2;
+        return ans;
     }
 }
