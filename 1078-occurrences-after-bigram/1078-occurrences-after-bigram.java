a343942935da487c8441a997940bb6632036febe
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-10-11 10:21:07 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (76.23%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
@@ -1,11 +1,38 @@
+// class Solution {
+//     public String[] findOcurrences(String text, String first, String second) {
+//         String[] words = text.split("\\s+");
+//         List<String> ans = new ArrayList<>();
+//         for (int i = 0; i < words.length-2; i+=1) {
+//             if (first.equals(words[i]) && second.equals(words[i+1]))
+//                 ans.add(words[i+2]);
+//         }
+//         return ans.toArray(new String[0]);
+//     }
+// }
+
 class Solution {
     public String[] findOcurrences(String text, String first, String second) {
-        String[] words = text.split("\\s+");
-        List<String> ans = new ArrayList<>();
-        for (int i = 0; i < words.length-2; i+=1) {
-            if (first.equals(words[i]) && second.equals(words[i+1]))
-                ans.add(words[i+2]);
+        int isFirst = 0, isSecond=0;
+        String[] words = text.split(" ");
+        int len = words.length;
+        String[] temp = new String[len];
+        int ind = 0;
+        for(int i=0; i<len; i++){
+            if(isFirst>0 && isSecond>0){
+                temp[ind++] = words[i];
+                isFirst--;isSecond--;
+            }
+            if(isFirst>0 && words[i].equals(second)){
+                isSecond++;
+            }else{
+                isFirst=0;
+            }
+            if(words[i].equals(first)){
+                isFirst++;
+            }
         }
-        return ans.toArray(new String[0]);
+        String[] ans = new String[ind];
+        for(int i=0; i<ind; i++) ans[i] = temp[i];
+        return ans;
     }
 }
