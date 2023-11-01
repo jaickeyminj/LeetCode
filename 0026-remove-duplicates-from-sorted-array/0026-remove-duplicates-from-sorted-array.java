25a05135c48431a674139eb7845c262b6190b2ea
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-01 20:54:09 +0530

    Time: 1 ms (96.51%), Space: 44 MB (30.10%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,24 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] A) {
         
-        // if (A.length<2) return A.length;
-        // int j=0;
-        // for (int i=0; i<A.length; i++)
-        //     if (A[i]!=A[j]){
-        //         A[++j]=A[i];
-        //     }
-        // return ++j;
+        if (A.length<2) return A.length;
+        int j=0;
+        for (int i=0; i<A.length; i++)
+            if (A[i]!=A[j]){
+                A[++j]=A[i];
+            }
+        return ++j;
         
-        Set<Integer> set = new TreeSet<>();
+//         Set<Integer> set = new TreeSet<>();
         
-        for(int i: A){
-            set.add(i);
-        }
-        int i=0;
-        for (Iterator<Integer> it = set.iterator(); it.hasNext(); ) {
-         Integer f = it.next();
-            A[i++]= f;
-    }
-        return set.size();
+//         for(int i: A){
+//             set.add(i);
+//         }
+//         int i=0;
+//         for (Iterator<Integer> it = set.iterator(); it.hasNext(); ) {
+//          Integer f = it.next();
+//             A[i++]= f;
+//     }
+//         return set.size();
 }
 }
