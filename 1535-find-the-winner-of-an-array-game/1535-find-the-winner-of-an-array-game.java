7e37f58f6c03719193cf8048876aac847fceef58
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-05 14:25:38 +0530

    Time: 15 ms (18.12%), Space: 58.2 MB (6.52%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -1,51 +1,51 @@
-// class Solution {
-//     public int getWinner(int[] arr, int k) {
-//         if(k>arr.length){
-//             int max = Integer.MIN_VALUE;
-//             for(int i: arr){
-//                 max = Math.max(max,i);
-//             }
-//             return max;
-//         }
-//         else{
-//             Map<Integer,Integer> hm = new HashMap<>();
-            
-//             int a = arr[0];
-//             int b = arr[1];
-            
-//             if(arr.length==2)
-//                 return Math.max(a,b);
-//             int winner = Math.max(a,b);
-//             hm.put(winner,hm.getOrDefault(winner,0)+1);
-//             if(hm.get(winner)==k)
-//                     return winner;
-//             for(int i=2;i<arr.length;i+=1){
-//                 a = winner;
-//                 b = arr[i];
-//                 winner = Math.max(a,b);
-//                 hm.put(winner,hm.getOrDefault(winner,0)+1);
-//                 if(hm.get(winner)==k)
-//                     return winner;
-//             }
-//             System.out.println(hm);
-//             return winner;
-//         }
-//     }
-// }
-
 class Solution {
     public int getWinner(int[] arr, int k) {
-        int win=0;
-        int ele=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>ele){
-                win=0;
-                ele=arr[i];
+        if(k>arr.length){
+            int max = Integer.MIN_VALUE;
+            for(int i: arr){
+                max = Math.max(max,i);
             }
-            if(++win==k){
-               break;
-            }
+            return max;
         }
-        return ele;
+        else{
+            Map<Integer,Integer> hm = new HashMap<>();
+            
+            int a = arr[0];
+            int b = arr[1];
+            
+            if(arr.length==2)
+                return Math.max(a,b);
+            int winner = Math.max(a,b);
+            hm.put(winner,hm.getOrDefault(winner,0)+1);
+            if(hm.get(winner)==k)
+                    return winner;
+            for(int i=2;i<arr.length;i+=1){
+                a = winner;
+                b = arr[i];
+                winner = Math.max(a,b);
+                hm.put(winner,hm.getOrDefault(winner,0)+1);
+                if(hm.get(winner)==k)
+                    return winner;
+            }
+            // System.out.println(hm);
+            return winner;
+        }
     }
 }
+
+// class Solution {
+//     public int getWinner(int[] arr, int k) {
+//         int win=0;
+//         int ele=arr[0];
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]>ele){
+//                 win=0;
+//                 ele=arr[i];
+//             }
+//             if(++win==k){
+//                break;
+//             }
+//         }
+//         return ele;
+//     }
+// }
