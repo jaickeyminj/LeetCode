cbd3cf9af638df92449acf7aee524941a48c35d6
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-05 14:18:40 +0530

    Time: 14 ms (18.12%), Space: 57.9 MB (7.25%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int getWinner(int[] arr, int k) {
+        if(k>arr.length){
+            int max = Integer.MIN_VALUE;
+            for(int i: arr){
+                max = Math.max(max,i);
+            }
+            return max;
+        }
+        else{
+            Map<Integer,Integer> hm = new HashMap<>();
+            
+            int a = arr[0];
+            int b = arr[1];
+            
+            if(arr.length==2)
+                return Math.max(a,b);
+            int winner = Math.max(a,b);
+            hm.put(winner,hm.getOrDefault(winner,0)+1);
+            if(hm.get(winner)==k)
+                    return winner;
+            for(int i=2;i<arr.length;i+=1){
+                a = winner;
+                b = arr[i];
+                winner = Math.max(a,b);
+                hm.put(winner,hm.getOrDefault(winner,0)+1);
+                if(hm.get(winner)==k)
+                    return winner;
+            }
+            System.out.println(hm);
+            return winner;
+        }
+    }
+}
