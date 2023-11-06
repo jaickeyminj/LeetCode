9aa48182a663ee906fe6460b961f449861530f9b
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-07 00:03:32 +0530

    Time: 0 ms (100.00%), Space: 74.7 MB (26.04%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,36 +1,38 @@
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         int V = (int)10e5;
+//         List<List<Integer>> am = new ArrayList<>();
+//         int maxi = 0;
+//         for(int e[]:edges){
+//             maxi = Math.max(maxi,Math.max(e[0],e[1]));
+//         }
+//         V= maxi+1;
+//         for(int i=0;i<V;i+=1){
+//             am.add(new ArrayList<Integer>());
+//         }
+//         for(int e[]:edges){
+//             addEdge(am,e[0],e[1]);
+//         }
+//         for(int i=0;i<V;i+=1){
+//             if(maxi-1 == am.get(i).size()){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+    
+//     public void addEdge(List<List<Integer>> am, int s, int d) {
+//         am.get(s).add(d);
+//         am.get(d).add(s);
+//     }
+// }
+
 class Solution {
     public int findCenter(int[][] edges) {
-        int V = (int)10e5;
-        List<List<Integer>> am = new ArrayList<>();
-        int maxi = 0;
-        for(int e[]:edges){
-            maxi = Math.max(maxi,Math.max(e[0],e[1]));
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
         }
-        V= maxi+1;
-        for(int i=0;i<V;i+=1){
-            am.add(new ArrayList<Integer>());
-        }
-        for(int e[]:edges){
-            // if(am.get(e[0])==null) am.add(new ArrayList<Integer>());
-            // if(am.get(e[1])==null) am.add(new ArrayList<Integer>());
-            addEdge(am,e[0],e[1]);
-        }
-        int max = Integer.MIN_VALUE;
-        
-        for(int i=0;i<V;i+=1){
-            max = Math.max(max,am.get(i).size());
-        }
-        // System.out.println(max);
-        for(int i=0;i<V;i+=1){
-            if(max == am.get(i).size()){
-                return i;
-            }
-        }
-        return -1;
-    }
-    
-    public void addEdge(List<List<Integer>> am, int s, int d) {
-        am.get(s).add(d);
-        am.get(d).add(s);
     }
 }
