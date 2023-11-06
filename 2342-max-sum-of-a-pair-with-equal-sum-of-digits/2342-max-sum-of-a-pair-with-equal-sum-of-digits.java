cbcc5a5cac5b464f4588b6ada5e88f99e4710f12
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-06 20:27:03 +0530

    Time: 21 ms (97.34%), Space: 56.5 MB (80.50%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,59 +1,86 @@
+// class Solution {
+//     public int maximumSum(int[] nums) {
+//         Map<Integer,List<Integer>> map = new HashMap<>();
+        
+//         for(int i=0;i<nums.length;i+=1){
+//             int key = calculateSum(nums[i]);
+//             if(map.containsKey(key)){
+//                 List<Integer> al = map.get(key);
+//                 al.add(nums[i]);
+//                 map.put(key,new ArrayList<>(al));
+//             }else{
+//                 List<Integer> list = new ArrayList<>();
+//                 list.add(nums[i]);
+//                 map.put(key,list);
+//             }
+//         }
+//         // System.out.println(map);
+//         PriorityQueue<Map.Entry<Integer,List<Integer>>> pq = new PriorityQueue<>(
+//         (a,b)-> {
+//             // if(a.getValue().size()>=2 && b.getValue().size()>=2){
+//             //     // int sum1 = a.getValue().get(0) + a.getValue().get(1);
+//             //     // int sum2 = b.getValue().get(0) + b.getValue().get(1);
+//             //     return b.getValue().size()- a.getValue().size();
+//             // }
+//             return b.getValue().size() - a.getValue().size();
+//         });
+        
+//         for(Map.Entry<Integer,List<Integer>> entry: map.entrySet()){
+//             pq.offer(entry);
+//         }
+        
+//         // int pair = 2;
+//         int ans = -1;
+//         int max = Integer.MIN_VALUE;
+//         while(!pq.isEmpty()){
+//             List<Integer> al = pq.poll().getValue();
+//             if(al.size()==1)
+//                 break;
+//             else if(al.size()>1){
+//                 int j = Collections.max(al); //max1
+//                 al.remove(al.indexOf(j));
+//                 int max2 = Collections.max(al); //max2
+//                 max = Math.max(j + max2,max); 
+//                 ans=0;
+//                 // break;
+//             }
+//         }
+//         return ans==-1? ans : max;
+//     }
+    
+//     public int calculateSum(int n){
+//         int sum = 0;
+//         while(n!=0){
+//             sum+=n%10;
+//             n/=10;
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int maximumSum(int[] nums) {
-        Map<Integer,List<Integer>> map = new HashMap<>();
-        
-        for(int i=0;i<nums.length;i+=1){
-            int key = calculateSum(nums[i]);
-            if(map.containsKey(key)){
-                List<Integer> al = map.get(key);
-                al.add(nums[i]);
-                map.put(key,new ArrayList<>(al));
-            }else{
-                List<Integer> list = new ArrayList<>();
-                list.add(nums[i]);
-                map.put(key,list);
-            }
+        int[] map = new int[82]; //9*9 = 81
+        int result = -1;
+
+        for (int item : nums) {
+            int key = getNumberTotal(item);
+
+            if (map[key] != 0)
+                result = Math.max(result, map[key] + item);
+            map[key] = Math.max(map[key], item);
         }
-        // System.out.println(map);
-        PriorityQueue<Map.Entry<Integer,List<Integer>>> pq = new PriorityQueue<>(
-        (a,b)-> {
-            // if(a.getValue().size()>=2 && b.getValue().size()>=2){
-            //     // int sum1 = a.getValue().get(0) + a.getValue().get(1);
-            //     // int sum2 = b.getValue().get(0) + b.getValue().get(1);
-            //     return b.getValue().size()- a.getValue().size();
-            // }
-            return b.getValue().size() - a.getValue().size();
-        });
-        
-        for(Map.Entry<Integer,List<Integer>> entry: map.entrySet()){
-            pq.offer(entry);
-        }
-        
-        // int pair = 2;
-        int ans = -1;
-        int max = Integer.MIN_VALUE;
-        while(!pq.isEmpty()){
-            List<Integer> al = pq.poll().getValue();
-            if(al.size()==1)
-                break;
-            else if(al.size()>1){
-                int j = Collections.max(al); //max1
-                al.remove(al.indexOf(j));
-                int max2 = Collections.max(al); //max2
-                max = Math.max(j + max2,max); 
-                ans=0;
-                // break;
-            }
-        }
-        return ans==-1? ans : max;
+
+        return result;
     }
-    
-    public int calculateSum(int n){
-        int sum = 0;
-        while(n!=0){
-            sum+=n%10;
-            n/=10;
+
+    private int getNumberTotal(int num) {
+        int result = 0;
+        while (num > 0) {
+            result += num % 10;
+            num /= 10;
         }
-        return sum;
+
+        return result;
     }
 }
