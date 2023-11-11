68aec8bc40aa79120ed35ac7751d7fd76057c6c9
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-12 01:15:12 +0530

    Time: 3 ms (96.34%), Space: 43.5 MB (84.17%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,23 +1,33 @@
 class Solution {
     public String finalString(String s) {
-        Stack<Character> st = new Stack<>();
-        Stack<Character> st2 = new Stack<>();
-        for(char c: s.toCharArray()){
-            if(c!='i'){
-                st.push(c);
-            }
-            else{
-                while(!st.isEmpty()){
-                    st2.push(st.pop());
-                }
-                st =(Stack<Character>) st2.clone();
-                st2 = new Stack<>();
-            }
+        // Stack<Character> st = new Stack<>();
+        // Stack<Character> st2 = new Stack<>();
+        // for(char c: s.toCharArray()){
+        //     if(c!='i'){
+        //         st.push(c);
+        //     }
+        //     else{
+        //         while(!st.isEmpty()){
+        //             st2.push(st.pop());
+        //         }
+        //         st =(Stack<Character>) st2.clone();
+        //         st2 = new Stack<>();
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // while(!st.isEmpty()){
+        //     sb.append(st.pop());
+        // }
+        // return ""+sb.reverse().toString();
+        
+        
+        StringBuilder ans=new StringBuilder();
+       
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='i') ans.reverse();
+            else ans.append(s.charAt(i));
         }
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-        return ""+sb.reverse().toString();
+        return ans.toString();
     }
 }
