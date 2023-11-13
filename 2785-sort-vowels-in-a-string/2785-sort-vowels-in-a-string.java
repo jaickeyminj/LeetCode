545c81bc6cb86abd0fe6cb47d4eefb68adbeb66e
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-13 18:50:14 +0530

    Time: 65 ms (37.38%), Space: 46.2 MB (53.61%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public String sortVowels(String s) {
-//         97, 101, 105, 111, and 117,
-//         65, 69, 73, 79, and 85
+//         97, 101, 105, 111, and 117 (Lower-Case)
+//         65, 69, 73, 79, and 85 (Upper-Case)
         PriorityQueue<Character> pq = new PriorityQueue<>();
         PriorityQueue<Character> pq2 = new PriorityQueue<>();
         for(char c: s.toCharArray()){
@@ -13,25 +13,19 @@ class Solution {
             }
         }
         
-        // System.out.println(pq+"\n"+pq2);
-        
         StringBuilder sb = new StringBuilder();
         for(char c: s.toCharArray()){
             if(c==97 || c== 101 || c==105 || c== 111 || c== 117 
                ||c== 65 || c== 69 || c== 73 || c== 79 ||c== 85){
                 if(pq2.size()>0){
                     sb.append(pq2.poll());
-                    // System.out.println(pq2.size());
-                    // continue;
                 }
                 else if(pq.size()>0){
                     sb.append(pq.poll());
-                    // continue;
                 }
             }
             else
                 sb.append(c);
-            // System.out.println(sb.toString() +" -> "+ pq +" <- "+ pq2);
         }
         
         return sb.toString();
