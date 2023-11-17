154b5d0fffece2efde76c517350b773f2acf3c63
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-17 19:36:32 +0530

    Time: 5 ms (100.00%), Space: 44.3 MB (26.56%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        int i = 0;
+        /*int i = 0;
         int j= s.length()-1;
         StringBuilder sb = new StringBuilder();
         char a[] = s.toCharArray();
@@ -22,6 +22,19 @@ class Solution {
             j-=1;
         }
         
-        return new String(a);
+        return new String(a);*/
+        
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left <= right) {
+            if (chars[left] != chars[right]) {
+                    chars[left] = chars[right] = (char) Math.min(chars[left], chars[right]);
+            }
+            left++;
+            right--;
+        }
+
+        return new String(chars);
     }
 }
