b7ec47b42e918b0fde2c2a1fbbc06325d1deafc5
jaickeyminj <95216865+jaickeyminj@users.noreply.github.com>
2023-11-19 18:31:20 +0530

    Time: 7 ms (100.00%), Space: 54.3 MB (80.26%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,15 +1,35 @@
 class Solution {
+    
+    public int reductionOperations(int[] nums) {
+        int[] count = new int[5 * 10000 + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        int sum = 0;
+        int distinctCount = 0;
+        for (int numCount : count) {
+            if (numCount != 0) {
+                sum += distinctCount * numCount;
+                distinctCount++;
+            }
+        }
+
+        return sum;
+    }
+}
+
+/*class Solution {
     public int reductionOperations(int[] nums) {
         Arrays.sort(nums);
         int cnt = 0;
         int res = 0;
         for(int i=0;i<nums.length-1;i+=1){
             if(nums[i]!=nums[i+1]){
-                // nums[i+1] = nums[i];
                 cnt+=1;
             }
             res +=cnt;
         }
         return res;
     }
-}
+}*/
