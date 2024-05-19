9418ba44bd4dc3d7fdcbc9c8e6aad3da38f1e95b
Jaickey Joy Minj <95216865+jaickeyminj@users.noreply.github.com>
2024-05-19 11:54:12 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==1)
+            return true;
+        int len = nums.length;
+        for(int i=1;i<len;i+=1){
+            if((nums[i]%2==0)^(nums[i-1]%2==0))
+                continue;
+            else
+                return false;
+        }
+        
+        return true;
+    }
+}
