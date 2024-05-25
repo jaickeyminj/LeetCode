2ff5ade0d96f03ae7bc2651100a2502473e9d56f
Jaickey Joy Minj <95216865+jaickeyminj@users.noreply.github.com>
2024-05-25 17:45:30 +0530

    Time: 1 ms (66.49%), Space: 40.6 MB (57.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1) {
+            int curr = n;
+            int sum = 0;
+            while (curr > 0) {
+                sum += (curr % 10) * (curr % 10);
+                curr /= 10;
+            }
+
+            if (!set.add(sum)) {
+                return false;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
