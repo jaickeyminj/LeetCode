e14cf44465ed2fbd1891d902fd09bbce8e33d944
Jaickey Joy Minj <95216865+jaickeyminj@users.noreply.github.com>
2024-05-26 17:08:53 +0530

    Time: 5 ms (98.12%), Space: 45.3 MB (50.35%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -1,10 +1,28 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
-        List<int[]> list = new LinkedList<>();
+        // Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        // List<int[]> list = new LinkedList<>();
+        // for (int[] p : people) {
+        //     list.add(p[1], p);
+        // }
+        // return list.toArray(new int[list.size()][]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (p1, p2) -> p1[0] != p2[0] ? p2[0] - p1[0] : p1[1] - p2[1]);
+        
         for (int[] p : people) {
-            list.add(p[1], p);
+            pq.add(p);
         }
-        return list.toArray(new int[list.size()][]);    
+        
+        
+        // 2.
+        List<int[]> answer = new ArrayList<>();
+        
+        while (!pq.isEmpty()) {
+            int[] person = pq.poll();
+            answer.add(person[1], person);
+        }
+        
+        // 3.
+        return answer.toArray(new int[answer.size()][2]);  
     }
 }
