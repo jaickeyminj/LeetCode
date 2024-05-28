57879a786a0c5babb6db50a144290893e3b0fa11
Jaickey Joy Minj <95216865+jaickeyminj@users.noreply.github.com>
2024-05-28 17:29:33 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (80.54%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
@@ -1,22 +1,31 @@
 class Solution {
-    public int maximumStrongPairXor(int[] nums) {
-        int n[] = new int[nums.length*nums.length];
+    /*public int maximumStrongPairXor(int[] nums) {
+        // int n[] = new int[nums.length*nums.length];
         
-        int i=0;
-        
+        // int i=0;
+        int max = Integer.MIN_VALUE;
         for(int a:nums){
             for(int b: nums){
                 if(Math.abs(a-b)<=Math.min(a,b)){
-                    n[i]=a^b;
-                    i+=1;
+                    max = Math.max(a^b,max);
+                    // i+=1;
                 }
             }
         }
-        int max = Integer.MIN_VALUE;
-        for(int j:n){
-            max = Math.max(max,j);
+        return max;
+    }*/
+    
+    public int maximumStrongPairXor(int[] nums) {
+        int max = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int x = nums[i] ^ nums[j];
+                if (x > max && Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j])) {
+                    max = x;
+                }
+            }
         }
-        
         return max;
     }
 }
